1099bd527601376d04e77020d82bda8d76cd07c3
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2021-06-19 22:24:11 +0200

    Add html file

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
-const { response } = require('express');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT  = process.env.PORT || 4444;
 
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    // res.send("Hello world");
+    res.sendFile('index.html', {root: path.join(__dirname)});
 });
 
 app.use((req, res) => {
     res.status(404).json({
         error: "Not found!"
-    })
+    });
 });
 
 app.listen(PORT, () => {
