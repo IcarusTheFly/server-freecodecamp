0bb57ae7933ee7d1ad0b525a18deda127514e4c6
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2021-06-19 20:52:15 +0200

    Add console.log to the main endpoint

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const { response } = require('express');
 var express = require('express');
 var app = express();
 
-console.log("First log");
-console.log("Hello World");
-
 const PORT  = process.env.PORT || 4444;
 
 app.get('/', (req, res) => {
-    res.send("Hello world...");
+    console.log("Hello World");
+    res.send("Hello world");
 });
 
 app.use((req, res) => {
@@ -20,5 +18,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log("Connected to the server!");
-    console.log("Hello World");
 });
