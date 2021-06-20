d72ff373a17dd784cd893b16311259e9a1ce6d0b
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2021-06-20 17:40:30 +0200

    Add PORT env variable

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
 const PORT  = process.env.PORT || 4444;
 
 app.get('/', (req, res) => {
     // res.send("Hello world");
     res.sendFile('index.html', {root: path.join(__dirname)});
+});
+
+app.get('/demo', (req, res) => {
+    res.status(200).end("This is just a demo");
 });
 
 app.use((req, res) => {
